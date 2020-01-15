a700585537d9ac310a2b7c353af6634ab9ad2700
juan <oqundo.juan@system-in-motion.com>
2020-01-15 17:47:25 +0800

    first commit

---
+++ src/test/java/com/jp/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
